41561d4ea3a6555b9ba858615a1d0cb92728ba42
jigyasha1234 <jigrani0000@gmail.com>
2025-10-04 13:18:49 +0530

    correcting error

--- xcountries/src/App.jsx
+++ xcountries/src/App.jsx
@@ -2,19 +2,23 @@ import Card from './card';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [cardData, setCardData] = useState(null);
-  const fetchCardData = async () => {
-    const response = await fetch('https://xcountries-backend.labs.crio.do/all');
-    const data = await response.json();
-    setCardData(data);
-  }
+    const [cardData, setCardData] = useState(null);
 
-  useEffect(() => {
-    try{
-    fetchCardData();
-    }catch{
+  const fetchCardData = async () => {
+    try {
+      const response = await fetch('https://xcountries-backend.labs.crio.do/all');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setCardData(data);
+    } catch (error) {
       console.error("Error fetching data: ", error);
     }
+  };
+
+  useEffect(() => {
+    fetchCardData();
   }, []);
 
   return (
